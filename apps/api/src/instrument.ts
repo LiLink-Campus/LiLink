6e10b6c512a834ba3492ac18a744aed3d716513e
s3d-i <bing@sed-i.org>
2026-06-02 16:43:20 +0800

    setup backend sentry

---
+++ apps/api/src/instrument.ts
@@ -0,0 +1,12 @@
+import * as Sentry from '@sentry/nestjs';
+import { env } from './config/env';
+
+Sentry.init({
+  dsn: env.SENTRY_DSN || undefined,
+  enabled: env.SENTRY_DSN.length > 0,
+  environment: env.APP_ENV,
+  release: env.SENTRY_RELEASE || undefined,
+  tracesSampleRate: env.SENTRY_TRACES_SAMPLE_RATE,
+  enableLogs: env.SENTRY_ENABLE_LOGS,
+  sendDefaultPii: env.SENTRY_SEND_DEFAULT_PII,
+});
